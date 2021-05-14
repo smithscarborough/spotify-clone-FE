297b18caa213c6134facce977dc5604f35fef64c
Smith Scarborough <smithscarborough@gmail.com>
2021-05-14 15:20:56 -0500

    add /login to end of redirect_uri in Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Container } from 'react-bootstrap'
 
-const AUTH_URL = "https://accounts.spotify.com/authorize?client_id=ad20fed2ce054d9e9b600324f79d8296&response_type=code&redirect_uri=https://spotify-by-smith.netlify.app/&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
+const AUTH_URL = "https://accounts.spotify.com/authorize?client_id=ad20fed2ce054d9e9b600324f79d8296&response_type=code&redirect_uri=https://spotify-by-smith.netlify.app/login&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
 
 export default function Login() {
     return (
